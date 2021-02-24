5d142ed7ff534e141a747ff81faf6003d5b92931
Samuel <Samueljambo81@gmail.com>
2021-02-23 22:17:18 -0500

    added project file

---
+++ Assets/_Scripts/HealthBarScreenSpaceController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class HealthBarScreenSpaceController : MonoBehaviour
+{
+    [Header("Health Properties")]
+    [Range(0, 100)]
+    public int currentHealth = 100;
+    [Range(1, 100)]
+    public int maximumHealth = 100;
+
+    private Slider healthBarSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBarSlider = GetComponent<Slider>();
+        currentHealth = maximumHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        healthBarSlider.value -= damage;
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            healthBarSlider.value = 0;
+            currentHealth = 0;
+        }
+    }
+
+    public void Reset()
+    {
+        healthBarSlider.value = maximumHealth;
+        currentHealth = maximumHealth;
+    }
+}
